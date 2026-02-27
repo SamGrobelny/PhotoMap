b06d7785069b510726f633e89d02db7ee357e7b4
Johanan Abraham <johanan.abraham@hotmail.com>
2026-02-26 20:45:10 -0500

    Initial Xcode project setup

---
+++ PhotoMap/PhotoMap/PhotoMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMapApp.swift
+//  PhotoMap
+//
+//  Created by Johanan Abraham on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
